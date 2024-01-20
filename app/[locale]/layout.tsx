df8407e40636937126c6c6e6deb5bf133c8b66ec
Lauri Ahlfors <ahlfroslauri@gmail.com>
2024-01-20 15:05:37 +0200

    feat: add functionality to use updated i18n functions

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/globals.css';
 import Nav from '@/components/nav';
 import { Locale, i18nConfig } from '@/i18n';
-import { getTranslation } from '@/lib/i18n/loadTranslations';
+import getTranslation from '@/lib/i18n/getTranslation';
 import { GeistSans } from 'geist/font/sans';
 import type { Metadata } from 'next';
 
@@ -10,24 +10,23 @@ export const metadata: Metadata = {
 };
 
 export async function generateStaticParams() {
-  return i18nConfig.locales.map((locale) => ({ locale: locale }));
+  return i18nConfig.locales.map((locale: Locale) => ({ locale: locale }));
 }
 
-export default async function RootLayout({
-  children,
-  params,
-}: {
+type Props = {
   children: React.ReactNode;
   params: {
     locale: Locale;
   };
-}) {
-  const t = await getTranslation(params.locale);
+};
+
+export default async function RootLayout({ children, params }: Props) {
+  const translation = await getTranslation(params.locale);
 
   return (
     <html lang={params.locale} className={GeistSans.className}>
       <body>
-        <Nav t={t} />
+        <Nav translation={translation} />
         <main className="px-8 pt-16 lg:px-96">{children}</main>
       </body>
     </html>
--- components/nav.tsx
+++ components/nav.tsx
@@ -1,14 +1,14 @@
 import LocaleSelector from '@/components/locale-selector';
-import { TranslationKeys } from '@/lib/i18n/loadTranslations';
+import { TranslationObejct } from '@/lib/i18n/loadTranslation';
 
 interface Props {
-  t: TranslationKeys;
+  translation: TranslationObejct;
 }
 
-export default function Nav({ t }: Props) {
+export default function Nav({ translation }: Props) {
   return (
     <nav className="absolute flex h-16 w-full items-center justify-end border-b border-b-neutral-200 bg-white px-8 lg:px-96">
-      <LocaleSelector message={t('nav.message')} />
+      <LocaleSelector message={translation('nav.locale-selector.message')} />
     </nav>
   );
 }
